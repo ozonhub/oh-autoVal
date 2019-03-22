c9322dfabe45cafdb34ed0239fd1edf4d1a64c3e
ozonhub <32862889+ozonhub@users.noreply.github.com>
2019-03-22 12:15:29 +0530

    Update oh-autoval-script.js

--- js/oh-autoval-script.js
+++ js/oh-autoval-script.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
- alert("Oh-autoval API is deprecated and no more available. You can use ClientVal-API v1.9.3. More info : https://clientval.ml");
-});
+ alert("Oh-autoval API is deprecated and no more available. You can use ClientVal-API v1.9.3. More info : https://clientval.ml or dial +91-8592962849");
+}); 
